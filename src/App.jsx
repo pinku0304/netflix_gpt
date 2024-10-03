c608140a2f3967317063ea07fab54a0cb45b8344
pinku0304 <priyankahm1994@gmail.com>
2024-10-03 12:49:09 +0530

    newchange

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
+import Body from "./components/Body";
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl text-black">Hello World</h1>
-    </>
-  );
+  return <Body />;
 }
 
 export default App;
---
+++ src/components/Body.jsx
@@ -0,0 +1,27 @@
+import Login from "./Login";
+import Browse from "./Browse";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+  return (
+    <div className="container pt-5">
+      {/* <Login />
+      <Browse /> */}
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+};
+export default Body;
